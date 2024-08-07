b35841341612864462ad76eecd3f329833f6acd1
Kavita Jakhar <Kavya21@gmail.com>
2024-08-07 17:00:11 +0530

    changed some styles

--- components/About.js
+++ components/About.js
@@ -1,11 +1,11 @@
 const About = () => {
     return (
-      <section className="about-section">
+      <div className="about-section">
         <h2 className="about-heading">DISCOVER OUR PRODUCTS</h2>
         <p className="about-description">
         Lorem ipsum dolor sit amet consectetur. Amet est posuere rhoncus scelerisque. Dolor integer scelerisque nibh amet mi ut elementum dolor.
         </p>
-      </section>
+      </div>
     );
   }
   
